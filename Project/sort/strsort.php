71da76bda334279bf2690e8b7bd1eb934155a36a
Roman-cmd <53526773+Roman-cmd@users.noreply.github.com>
2024-08-22 16:26:27 +0700

    complite

--- Project/sort/strsort.php
+++ Project/sort/strsort.php
@@ -3,9 +3,7 @@ function bubbleSort(&$array) {
     $n = count($array);
     for ($i = 0; $i < $n; $i++) {
         for ($j = 0; $j < $n - 1 - $i; $j++) {
-            // Сравниваем строки
             if ($array[$j] > $array[$j + 1]) {
-                // Меняем местами, если порядок неправильный
                 $temp = $array[$j];
                 $array[$j] = $array[$j + 1];
                 $array[$j + 1] = $temp;
@@ -14,9 +12,8 @@ function bubbleSort(&$array) {
     }
 }
 
-// Пример использования
+
 $array = ["banana", "apple", "orange", "grape"];
 bubbleSort($array);
 
-// Вывод отсортированного массива
 print_r($array);
